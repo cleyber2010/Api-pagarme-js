89d634f6fb58345a5b1f665025a7add5413fe9d1
Cleyber Matos <cleyber.fernandes@gmail.com>
2024-11-29 07:20:38 -0300

    Creating a request to create a customer in pagarme

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+//Criando cliente na PAGAR.ME
+const apiKey = window.btoa("sk_test_2b51df83e9f04d1189f484d5594ad1f1");
+
+function createCustomer() {
+
+    const data = {
+        name: document.getElementById("name").value,
+        document: document.getElementById("document").value,
+        document_type: document.getElementById("document_type").value,
+        type: document.getElementById("type").value        
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            'content-type': "application/json",
+            'Access-Control-Allow-Origin': '*',
+            authorization: 'Basic c2tfdGVzdF8yYjUxZGY4M2U5ZjA0ZDExODlmNDg0ZDU1OTRhZDFmMTpza190ZXN0XzJiNTFkZjgzZTlmMDRkMTE4OWY0ODRkNTU5NGFkMWYx'
+        }, 
+        body: JSON.stringify(data)
+    }
+    
+    fetch("https://api.pagar.me/core/v5/customers", options)
+        .then((res) => {
+            return res.json
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+document.querySelector("#button").addEventListener("click", (e) => {
+    e.preventDefault();
+    createCustomer();
+});
